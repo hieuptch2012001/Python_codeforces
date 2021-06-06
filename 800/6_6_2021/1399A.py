44af7bf3b24bf97a858ea2e5e262789e82f3be20
Pham_Hieu <hieuptch2012001@fpt.edu.vn>
2021-06-06 08:09:57 +0700

    update file 1399A.py

--- 800/6_6_2021/1399A.py
+++ 800/6_6_2021/1399A.py
@@ -2,7 +2,7 @@ def main():
     n = int(input())
     for i in range(n):
         a = int(input())
-        b = [int(x) for x in input().split()]
+        b = set([int(x) for x in input().split()])
         if (max(b) - min(b)) < len(b):
             print("YES")
         else:
